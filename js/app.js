255964cd5cb96fabfd8db3e6e2bfa3a4462ebe7c
Михаил <rivixxx@gmail.com>
2022-02-14 15:13:47 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider__left').slick({
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay:true,
+    });
+    // используй маскединпут на jquery
+    $("#phone").mask("+7(999) 999-9999");
+});
